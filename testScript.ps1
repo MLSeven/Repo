0fe4a09cab53084b176064191cbbd3441eb7348d
MLSeven <steve.potts@mlseven.co.uk>
2021-04-28 10:47:48 +0100

    test

--- testScript.ps1
+++ testScript.ps1
@@ -1,17 +1,13 @@
-# Test Powershell after branch rename
-$ScriptFile = Get-Item $myinvocation.Mycommand.Path
-$ScriptRoot = Split-Path -Path $myinvocation.Mycommand.Path -Parent
-$ScriptName = $ScriptFile.BaseName
-$subScript = join-path -path $ScriptRoot -ChildPath "Scripts" 
-
-
-$data = [PSCustomObject]@{Fullname=$ScriptFile.fullname; Root=$ScriptRoot; Name=$ScriptName; subScripts = $subScript; subfile=""}
-
-$psFile1 = join-path -path $subScript -ChildPath "SubFile2.ps1"
-
-if (test-path -path $psFile1) {
-    $exit = invoke-expression $psFile1
+# Standard PS header
+$ScriptEnv = [PSCustomObject]@{
+    Name = $MyInvocation.Command.Name;
+    Root = $PSScriptRoot;
+    Path = $PSCommandPath;
+    isWindows = $IsWindows;
+    isLinux = $IsLinux;
+    isMacOS = $IsMacOS;
+    isCoreCLR = $ISCoreCLR;
+    isScriptBlock = ($MyInvocation.ScriptBlock -ne $null)
 }
-write-host $exit
-$data.subfile=$psFile1
-$data | convertto-json
+
+$ScriptEnv | convertto-json
