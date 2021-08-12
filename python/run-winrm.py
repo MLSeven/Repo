812dcd5232f0c370733b0a1bbd4aa804942e092d
spottsMorpheus <spotts@morpheusdata.com>
2021-08-12 13:25:27 +0100

    Added run-winrm.py

---
+++ python/run-winrm.py
@@ -0,0 +1,14 @@
+from winrm-pwsh import run_winrm
+from morpheuscypher import Cypher
+import sys
+
+server = morpheus['server']['fqdn']
+password = Cypher(morpheus=morpheus).get('secret/spotts')
+
+try:
+    output=run_winrm(server,"Test\spotts",password,"")
+    print(output)
+    sys.exit(0)
+except Exception as err:
+    print(f"winRm Exception {err}")
+    sys.exit(1)
