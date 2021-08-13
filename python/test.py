edfc9c788b1a9ef2c610b9fdfd44ba7462a2533b
spottsMorpheus <spotts@morpheusdata.com>
2021-08-13 11:36:45 +0100

    test update

--- python/test.py
+++ python/test.py
@@ -1,12 +1,27 @@
 import sys
 import os
 
-server = morpheus['server']['fqdn']
+try:
+    morpheus
+except NameError:
+    morpheus = None
+
+if morpheus:
+    server = morpheus['server']['fqdn']
+else:
+    server ="No Morpheus Context"
+
 #password = Cypher(morpheus=morpheus).get('secret/spotts')
 
 print(f"Python Test running in the server context {server}")
+print(f"__file__ {__file__}")
+
 for path in sys.path:
-    print(path)
+    print(f"path = {path}")
 
 for item, value in os.environ.items():
     print(f"Environment - {item}: {value}")
+
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#sys.path.append(BASE_PATH)
+print(f"Base Path {BASE_PATH}")
