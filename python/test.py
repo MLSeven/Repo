fedd1d8ff0cb5097a48675c30d928d64121363a2
spottsMorpheus <spotts@morpheusdata.com>
2021-08-13 16:52:53 +0100

    Test.py now matches local example

--- python/test.py
+++ python/test.py
@@ -1,11 +1,5 @@
 import sys
 import os
-#trap the situation where Morpheus is Launching the script via <stdin> and add cwd to path
-if (__file__ == "<stdin>"):
-    sys.path.append(os.getcwd())
-
-# import any private modules
-from winrmpwsh import winrm_pwsh
 
 try:
     morpheus
@@ -13,25 +7,18 @@ except NameError:
     morpheus = None
 
 if morpheus:
-    server = morpheus['server']['fqdn']
+    print("Running Python in Morpheus from a Local source")
 else:
-    server ="No Morpheus Context"
-
-#password = Cypher(morpheus=morpheus).get('secret/spotts')
-
-print(f"Python Test running in the server context {server}")
-print(f"__file__ {__file__}")
-
+    print("This script is not being run via Morpheus")
+  
+print("Current value of sys.path")  
 for path in sys.path:
-    print(f"path = {path}")
-
+    print(f"sys.path = {path}")
+    
+print("Current Environment")
 for item, value in os.environ.items():
     print(f"Environment - {item}: {value}")
 
+print(f"__file__ is {__file__}")
 print(f"os.getcwd() {os.getcwd()}")
-print(f"abspath(__file__) = {os.path.abspath(__file__)}")
-print(f"os.path.dirname(os.path.abspath(__file__)) = {os.path.dirname(os.path.abspath(__file__))}")
-print(f"os.path.dirname(os.path.dirname(os.path.abspath(__file__))) {os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}")
-BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#sys.path.append(BASE_PATH)
-print(f"Base Path {BASE_PATH}")
+print(f"os.path.abspath(__file__) = {os.path.abspath(__file__)}")
